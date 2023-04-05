7a06454982704180d8cef3a509a84d15a45b4dd5
Anglès d'Auriac JC <jeanchristian@anglesdauriac.fr>
2023-04-05 15:37:06 +0200

    bug du createur de libBoggle corrige

--- libBoggle.cpp
+++ libBoggle.cpp
@@ -4,6 +4,99 @@
 #include <ctype.h>
 #include "libBoggle.h"
 
+boggleSolver::boggleSolver(string dictSt)
+{
+    unsigned int i;
+    char buf[128];
+    FILE *f;
+    num_words = 0;
+
+    if (!(f = fopen((char*)dictSt.c_str(), "r")))
+    {
+        status = "error opening dictionary";
+        return;
+    }
+
+    while (fgets(buf, 128, f)) {
+        buf[strlen(buf)-1] = '\0';
+        for (i = 0; i < strlen(buf); i++)
+            buf[i] = tolower(buf[i]);
+        dict[num_words++] = strdup(buf);
+        if (num_words >= DICT_SIZE) {
+            status = "dictionary too large\n";
+            return;
+        }
+    }
+    qsort(dict, num_words, sizeof(char *), my_sort);
+    status = "OK";
+}     // FIN boggleSolver::boggleSolver(string dict)
+// ***********************************************************************
+
+boggleSolver::boggleSolver(vector<char*> dictCont)
+// flag==0 : c'est le NOM du fichier, flag=1 : c'est le contenu du fichier
+{
+    unsigned int i;
+    num_words = 0;
+    for(i=0;i<dictCont.size();i++)
+    {
+        char *buf = dictCont[i],*mot;
+        mot = (char*)malloc(strlen(buf)-1);
+        for (unsigned int j = 0; j < strlen(buf)-1; j++)
+            mot[j] = tolower(buf[j]);
+        mot[strlen(buf)-1]= '\0';
+        dict[num_words++] = mot;
+        if (num_words >= DICT_SIZE) {
+            status = "dictionary too large \n";
+            return;
+        }
+    }
+    qsort(dict, num_words, sizeof(char *), my_sort);
+    status = "OK";
+}     // FIN boggleSolver::boggleSolver(char* dictCont)
+// ***********************************************************************
+
+string boggleSolver::solve(string data)
+{
+    int i,j;
+    int len_min_affiche=4;
+    for(i=0;i<4;i++)
+    {
+        // char st[6];
+        //printf("%d ",i);fflush(stdout);
+        //if (fgets(st,6,stdin)==NULL)
+        //exit(0);
+        for(j=0;j<4;j++)
+        {
+            //letters[i][j] = tolower(st[j]);
+            letters[i][j] = tolower(data[4*i+j]);
+            graph[i][j] = NOT_SEEN;
+        }
+    }
+    // printf("Working...\n");
+    // fflush(stdout);
+    for (j = 0; j < BOX_SIZE; j++)
+        for (i = 0; i < BOX_SIZE; i++)
+            dfs(i, j, 0);
+    qsort( found, num_found, sizeof(char *), st_sort_bylen);
+
+    for( i=0;i< num_found;i++)
+        if (strlen(found[i])>=(unsigned int) len_min_affiche)
+            break;
+    int z = 0;
+    string ans = "";
+    char aux[1024];
+    for(i=0;i< num_found;i++)
+    {
+        sprintf(aux,"%s   (%d)\n",found[i],valeur[strlen(found[i])]);
+        ans += string(aux);
+        z += valeur[strlen(found[i])];
+    }
+    sprintf(aux,"gain= %d\n",z);
+    ans += string(aux);
+    return ans;
+}     // FIN string boggleSolver::solve(string data)
+// ***********************************************************************
+
 void boggleSolver::try_print(char *word)
 {
   int i;
@@ -19,7 +112,7 @@ void boggleSolver::try_print(char *word)
 
   found[num_found++] = strdup(word);
   //  printf("bog %s\n", word);
-}     // FIN  try_print()
+}     // FIN  void boggleSolver::try_print()
 // ***************************************************************
 
 void boggleSolver::output(int depth)
@@ -43,7 +136,7 @@ void boggleSolver::output(int depth)
     } else
       top = --cen;
   }
-}     // FIN  output()
+}     // FIN  void boggleSolver::output()
 // ***************************************************************
 
 void boggleSolver::dfs(int x, int y, int depth)
@@ -71,96 +164,8 @@ void boggleSolver::dfs(int x, int y, int depth)
   }
 
   graph[x][y] = NOT_SEEN;
-}     // FIN  dfs()
+}     // FIN  void boggleSolver::dfs()
 // ***************************************************************
-
-boggleSolver::boggleSolver(string dictSt)
-{
-  unsigned int i;
-  char buf[128];
-  FILE *f;
-  num_words = 0;
-
-  if (!(f = fopen((char*)dictSt.c_str(), "r")))
-  {
-    status = "error opening dictionary";
-    return;
-  }
-
-  while (fgets(buf, 128, f)) {
-    buf[strlen(buf)-1] = '\0';
-    for (i = 0; i < strlen(buf); i++)
-      buf[i] = tolower(buf[i]);
-    dict[num_words++] = strdup(buf);
-    if (num_words >= DICT_SIZE) {
-      status = "dictionary too large\n";
-      return;
-    }
-  }
-
-  qsort(dict, num_words, sizeof(char *), my_sort);
-  status = "OK";
-}     // FIN boggleSolver::boggleSolver(string dict)
-// ***********************************************************************
-
-boggleSolver::boggleSolver(vector<char*> dictCont)
-// flag==0 : c'est le NOM du fichier, flag=1 : c'est le contenu du fichier
-{
-  unsigned int i;
-  num_words = 0;
-  for(i=0;i<dictCont.size();i++)
-  {
-    char *buf = dictCont[i];
-    buf[strlen(buf)-1] = '\0';
-    dict[num_words++] = strdup(buf);
-  }
-  qsort(dict, num_words, sizeof(char *), my_sort);
-  status = "OK";
-}     // FIN boggleSolver::boggleSolver(char* dictCont)
-// ***********************************************************************
-
-string boggleSolver::solve(string data)
-{
-  int i,j;
-  int len_min_affiche=4;
-  for(i=0;i<4;i++)
-    {
-      // char st[6];
-      //printf("%d ",i);fflush(stdout);
-      //if (fgets(st,6,stdin)==NULL)
-      //exit(0);
-      for(j=0;j<4;j++)
-        {
-         //letters[i][j] = tolower(st[j]);
-         letters[i][j] = tolower(data[4*i+j]);
-         graph[i][j] = NOT_SEEN;
-        }
-    }
- // printf("Working...\n");
- // fflush(stdout);
-  for (j = 0; j < BOX_SIZE; j++)
-    for (i = 0; i < BOX_SIZE; i++)
-      dfs(i, j, 0);
-  qsort( found, num_found, sizeof(char *), st_sort_bylen);
-
-  for( i=0;i< num_found;i++)
-    if (strlen(found[i])>=(unsigned int) len_min_affiche)
-      break;
-  int z = 0;
-  string ans = "";
-  char aux[1024];
-  for(i=0;i< num_found;i++)
-    {
-      sprintf(aux,"%s   (%d)\n",found[i],valeur[strlen(found[i])]);
-      ans += string(aux);
-      z += valeur[strlen(found[i])];
-    }
-  sprintf(aux,"gain= %d\n",z);
-  ans += string(aux);
-  return ans;
-}     // FIN string boggleSolver::solve(string data)
-// ***********************************************************************
-
 
 int my_sort(const void *a, const void *b)
 {
